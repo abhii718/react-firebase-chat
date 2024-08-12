09cb35e4cbc0accf248397da9f7cb88288dbbf57
abhii718 <abhinaysingh47639@gamil.com>
2024-08-12 12:26:04 +0530

    chatlist userInfo chat detail title done

---
+++ src/components/list/chatList/ChatList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+// import "./chatList.css"
+
+const ChatList = () => {
+  return (
+    <div className='chatlist'>ChatList</div>
+  )
+}
+
+export default ChatList
---
+++ src/components/list/userInfo/UserInfo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./userInfo.css";  
+
+const UserInfo = () => {
+  return (
+    <div className="userinfo">
+      UserInfo
+    </div>
+  )
+}
+
+export default UserInfo
